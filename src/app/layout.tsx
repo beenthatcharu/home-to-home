7e75a4d17cdbe6e361dda810fd0b78844648b3ca
Abhisharma08 <aksharma223@gmail.com>
2025-12-16 12:14:09 +0530

    errors fixed

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,7 +3,7 @@ import './globals.css';
 import { Toaster } from "@/components/ui/toaster"
 
 export const metadata: Metadata = {
-  title: 'Home2Home',
+  title: 'Home2Home-Furniture and More.',
   description: '',
 };
 
